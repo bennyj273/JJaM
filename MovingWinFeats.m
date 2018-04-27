1446f74ea3cf0d4b96b272eab20ce72d625f20b0
jaimiecarlson <jaimiec@seas.upenn.edu>
2018-04-26 20:04:13 -0400

    Added PLS code

--- MovingWinFeats.m
+++ MovingWinFeats.m
@@ -1,13 +1,8 @@
 function [v] = MovingWinFeats(x, fs, winLen, winDisp, featFn)
-    length(x)/fs
-    fs
-    winLen
-    winDisp
-    featFn
-    numWins = floor(((length(x)/fs) - winLen)/winDisp)
-    v = zeros(numWins, 1)
-    winDispSamples = winDisp*fs
-    winLenSamples = winLen*fs
+    numWins = floor(((length(x)/fs) - winLen)/winDisp);
+    v = zeros(numWins, 1);
+    winDispSamples = winDisp*fs;
+    winLenSamples = winLen*fs;
     for i = 1:numWins
         sample = x(i*winDispSamples:i*winDispSamples+winLenSamples);
         v(i) = featFn(sample);

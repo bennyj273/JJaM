7bf7a04b953f24bfd68401e94275d2e3fcb111f4
jaimiecarlson <jaimiec@seas.upenn.edu>
2018-04-27 15:41:10 -0400

    SVM regression

--- BE521_svm_reg.m
+++ BE521_svm_reg.m
@@ -46,81 +46,49 @@ for i = 1:3
     ecog{i} = session_ecog{i}.data.getvalues(1:num_values_train, 1:numChannels(i));
 end
 
-%% Crossvalidation
-
-%Number of folds
-%fold_num=10;
-%Calculate number of samples per fold
-%n = num_values_train;
-%sample_num = n/fold_num;
-%folds = cell(1,fold_num);
-%Randommly populate the dataset indices in the folds 
-%vec=1:fold_num
-%for i = 1:fold_num
-%    start = num_values_train
-%    folds{i}=vec(start:(start+n-1));
-%end
-%Number of unique sample indices
-%disp(length(unique([folds{:}])));
-
 %% Get values
 for i = 1:3
     dg{i} = session_dg{i}.data.getvalues(1:270000, 1:5);
     ecog{i} = session_ecog{i}.data.getvalues(1:270000, 1:numChannels(i));
 end
 
-%% Filter the ecog data
-
-%sz = 10;
-%filt = ones(sz, 1)/sz;
-%for i = 1:3
-%    for ch = 1:numChannels(i)
-%        ecog{i}(:,ch) = conv(ecog{i}(:, ch), filt, 'same');
-%    end
-%end
-
-%% Butterworth filter on the ecog data
-
-%d = designfilt('lowpassfir', 'PassbandFrequency', 190, 'StopbandFrequency', 200, 'PassbandRipple', 1, 'StopbandAttenuation', 60, 'SampleRate', 1000);
-%ecog_filtered = ecog;
-
-%for i = 1:3
-%    for ch = 1:numChannels(i)
-%        ecog_filtered{i}(:,ch) = filtfilt(d, ecog{i}(:,ch));
-%    end
-%end
-
-%fs = 1000;
-%band = [50]/(fs/2); %80 - 50 Hz of cutoff
-%[f, e] = butter(2, band);
-%for i = 1:3
-%    for ch = 1:numChannels(i)
-%        ecog_filtered{i}(:,ch) = filtfilt(f, e, ecog{i}(:,ch));
-%    end
-%end
-
-%ecog = ecog_filtered;
 
 %% Calculate mean features - training
 samplingFrequency = 1000;
 windowLength = 0.1; %100 ms
-overlap = 0.05; %50 ms overlap
+overlap = 0.5*windowLength; %50 ms overlap
 windowDisp = windowLength - overlap;
 
-features = cell(3, max(numChannels), 7); %max cell length - adding a correlation band
+fingers = 5; 
+features = cell(3, max(numChannels), 11); %max cell length - adding a correlation band
+output = cell(3, 5);
 %Features = 1 mean, 2-6 the 5 frequency bands
 
 %Functions
 
+thresh = 15;
 %Take average for each sample
+mn = @(x) mean(norm(x)) > thresh;
+avg = @(x) mean(x)
 
-avg = @(x) mean(x); %Average of everything in the channel
+%Moving vs not moving
 
+windowLength = 0.1;
 for i = 1:3 %per subject
+    for f = 1:fingers
+        output{i, f} = [];
+        for j = 1:windowLength*samplingFrequency:size(dg{i}(:, f), 1)-windowLength*samplingFrequency
+            sample = dg{i}(j:j+windowLength*samplingFrequency, f);
+            output{i, f} = [output{i, f} mean(norm(sample)) > thresh];
+        end
+    end
     for ch = 1:numChannels(i) %per channel
-      features{i, ch, 1} = [MovingWinFeats(ecog{i}(:, ch), 1000, windowLength, windowDisp, avg); 0]; %This seems fake
+        features{i, ch, 1} = [MovingWinFeats(ecog{i}(:, ch), 1000, windowLength, windowDisp, avg); 0]; %This seems fake
     end
 end
+
+%% Grouping into moving vs not moving
+
 
 %% Calculate frequency features - training
 %Evaluate the spectrum at 1000/2 + 1 = 501 frequencies
@@ -145,13 +113,11 @@ for i = 1:3 %per channel
         [spec, ~, t] = spectrogram(ecog{i}(:, ch), windowLength*samplingFrequency, overlap*samplingFrequency, Fs);
         for band = 1:size(freqbands,1)
             features{i, ch, band+1} = abs(mean(spec(angfreqindices(band,:), :)))';
-
         end
     end
 end
 
 %% Put correlation features at the end
-
 
 %% Decimation - training
 dg_subsampled = cell(3, 1);
@@ -166,21 +132,23 @@ end
 %% Prediction - training
 predicted_pos = cell(3, 5);
 f_predictors = cell(3, 5);
-means = cell(3, 5, 6);
-stdevs = cell(3,5,6);
+means = cell(3, 5,9);
+stdevs = cell(3,5,9);
 BETAS = cell(3, 5);
+Mdls = cell(3, 5);
 
 for i = 1:3
     feats = [];
     for ch = 1:numChannels(i)
-        for f = 1:6
+        for f = 1:size(features,3) %number of features
             means{i,ch,f} = mean(features{i,ch,f});
             stdevs{i,ch,f} = std(features{i,ch,f});
             norm_features{i,ch,f} = (features{i,ch,f}-means{i,ch,f}) / stdevs{i,ch,f};
             sz = 2;
             filt = ones(sz, 1)/sz;
             norm_features{i, ch, f} = conv(norm_features{i, ch, f}, filt, 'same');
-            feats = [feats norm_features{i, ch, f}(1:5998)];
+            size(norm_features{i, ch, f})
+            feats = [feats norm_features{i, ch, f}(1:5398)];
         end   
     end
 
@@ -191,6 +159,7 @@ for i = 1:3
         N = 3; %time bins before
         M = size(feats,1) - N+1; %Total time bins
         nu = size(feats,2); %number of "neurons" or features
+        % 11 features on 62 channels = very inefficient
         R = zeros(M, 1);
         for j = 1:M
             R(j, 1) = 1;
@@ -204,51 +173,30 @@ for i = 1:3
             end
             R = [R matrix]; 
         end
-        pos = pos(N+2:end);
+       pos = pos(N+2:end);
             
-        f = mldivide((R'*R), (R'*pos));
-        f_predictors{i, finger} = f;
-        est_pos = R*f;
-        x = est_pos(1)*ones(N+1, 1);
-        est_pos = [x; est_pos];
-        
-          x = est_pos(1)*ones(N+1, 1);
-          est_pos = [x; est_pos];
-%         %Need to spline it back up to 300,000
-%          
-        est_pos = est_pos(1:6000);
-        est_pos_full = spline(0:50:299999, est_pos, 0:1:299999);
-        predicted_pos{i, finger} = est_pos_full;
+       [Mdl, FitInfo] = fitrlinear(R, pos, 'Regularization', 'lasso', 'PassLimit', 10, 'Solver', 'asgd')
+       %PassLimit 5: testcorr 0.5308
+       %PassLimit 7: testcorr 0.5434, 0.5533 (submitted one)
+       %PassLimit 10:testcorr 0.5563 (still iteration limitexceeded)
+       %Passlimit 10 and asgd: 0.5620 (still iteration limit exceeded)
+       Mdls{i, finger} = Mdl;
+       est_pos = predict(Mdl, R);
+       x = est_pos(1)*ones(N+2, 1);
+       est_pos = [x; est_pos];
+       est_pos_full = spline(0:50:270000, est_pos, 0:1:270000);
+       predicted_pos{i, finger} = est_pos_full;
+       %Lasso
     end
 end
 %% Do some evaluation
 testcorr = 0;
 for i = 1:3
     for ch = 1:5
-        testcorr  = testcorr + corr(predicted_pos{i,ch}', dg{i}(:,ch));
+        testcorr  = testcorr + corr(predicted_pos{i,ch}(1:end-1)', dg{i}(:,ch));
     end
 end
 testcorr/15
-%%
-disp('-')
-sz = 300;
-filt = ones(sz, 1)/sz;
-filtered_predicted_pos = cell(3,5);
-totalcorr = 0;
-for i = 1:3
-    for ch = 1:5
-        filtered_predicted_pos{i, ch} = conv(predicted_pos{i,ch}', filt, 'same');
-    end
-end
-
-fs = 1000;
-band = [0.6 1.5]/(fs/2); %80 - 50 Hz of cutoff
-[f, e] = butter(1, band,'stop');
-for i = 1:3
-    for fing = 1:5
-        predpos_filtfilt{i,fing} = filtfilt(f, e, filtered_predicted_pos{i,fing});
-    end
-end
 
 %% Do some evaluation
 totalcorr = 0;
@@ -262,24 +210,6 @@ totalcorr = totalcorr/15
 
 %% Calculate testing data from f_predictors - testing
 
-%sz = 10;
-%filt = ones(sz, 1)/sz;
-%for i = 1:3
-%    for ch = 1:numChannels(i)
-%        ecog_leaderboard{i}(:,ch) = conv(ecog_leaderboard{i}(:, ch), filt, 'same');
-%    end
-%end
-
-%fs = 1000;
-%band = [50]/(fs/2); %80 - 50 Hz of cutoff
-%[f, e] = butter(2, band);
-%for i = 1:3
-%    for ch = 1:numChannels(i)
-%        ecog_filtered{i}(:,ch) = filtfilt(f, e, ecog{i}(:,ch));
-%    end
-%end
-
-%ecog = ecog_filtered;
 
 %Calculate all metrics again including R matrices
 
@@ -288,7 +218,7 @@ windowLength = 0.1; %100 ms
 overlap = 0.05; %50 ms overlap
 windowDisp = windowLength - overlap;
 
-features_leaderboard = cell(3, max(numChannels), 6); %max cell length
+features_leaderboard = cell(3, max(numChannels), 11); %max cell length
 %Features = 1 mean, 2-6 the 5 frequency bands
 
 %Functions
@@ -333,7 +263,7 @@ predicted_pos_leaderboard = cell(3, 1);
 for i = 1:3
     feats = [];
     for ch = 1:numChannels(i)
-        for f = 1:6
+        for f = 1:size(features, 3)
             norm_features_leaderboard{i,ch,f} = (features_leaderboard{i,ch,f}-means{i,ch,f})/stdevs{i,ch,f};
             sz = 2;
             filt = ones(sz, 1)/sz;
@@ -363,11 +293,12 @@ for i = 1:3
         %x = est_pos(1)*ones(N, 1);
         %est_pos = [x; est_pos];
         %Need to spline it back up to 300,000
-           
-       est_pos = R*f_predictors{i, finger}; %Make predictions
-        x = est_pos(1)*ones(N, 1);
+        Mdl = Mdls{i,finger};
+        est_pos = predict(Mdl, R);
+        x = est_pos(1)*ones(N+2, 1);
         est_pos = [x; est_pos];
-        %Need to spline it back up to 300,000
+        %est_pos_full = spline(0:50:270000, est_pos, 0:1:270000);
+        %predicted_pos{i, finger} = est_pos_full;
         est_pos_full = spline(0:50:147499, est_pos, 0:1:147499);
         prediction = [prediction est_pos_full'];
     end
@@ -383,6 +314,7 @@ for i = 1:3
         predicted_pos_leaderboard_filtered{i}(:,ch) = conv(predicted_pos_leaderboard{i}(1:end, ch), filt, 'same');
     end
 end
+%%
 
 predpos_leaderboard_filtfilt= cell(3,1);
 fs = 1000;
